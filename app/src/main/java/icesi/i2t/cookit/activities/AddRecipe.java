acb25b7f54fa5206252f945373e682fecc8eda40
JuanEsteban <juanes-2504@hotmail.com>
2018-12-01 12:59:59 -0500

    Add Recipe 12:59

---
+++ app/src/main/java/icesi/i2t/cookit/activities/AddRecipe.java
@@ -0,0 +1,15 @@
+package icesi.i2t.cookit.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import icesi.i2t.cookit.R;
+
+public class AddRecipe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_recipe);
+    }
+}
